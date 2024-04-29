06587b1fa6ea2793cd2bb01603efff18d0c60441
Riikka Alanen <riikka.alanen@kuubi.fi>
2024-04-29 14:49:46 +0300

    Add constants

---
+++ src/constants.js
@@ -0,0 +1,12 @@
+export const scaleFactor = 4;
+
+export const dialogueData = {
+    pc: `Where the coding happens.`,
+    tv: `test text`,
+    bed: `test text`,
+    resume: `a summary of skills and experiences.`,
+    projects: `showcasing a portfolio of work.`,
+    library: `a collection of knowledge and stories.`,
+    "sofa-table": `a place for conversations and coffee.`,
+    "cs-degree": `cs-degree, a testament to dedication and learning.`
+}
